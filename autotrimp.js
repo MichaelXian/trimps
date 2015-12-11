5f6e945c0b9989d3515d0a0f222bd856cb99117a
Belaith <quanti_T@web.de>
2015-12-11 19:19:45 +0100

    sendTrimpsToWork using percentage of free workers
    sendTrimpsToWork now uses 10% of free workers minus global.buyAmt round up to the next integer.

--- autotrimp.js
+++ autotrimp.js
@@ -277,9 +277,11 @@ function buyGemCheapestHousing() {
 // send trimps to work if there are a lot waiting around!!
 function sendTrimpsToWork() {
 	var workspaces = Math.ceil(game.resources.trimps.realMax() / 2) - game.resources.trimps.employed;
-	if (workspaces > 10 + game.global.buyAmt) {
+	if (workspaces > game.global.buyAmt) {
+		var buyAmt = game.global.buyAmt;
+		game.global.buyAmt = Math.ceil((workspaces- game.global.buyAmt)*0.1);
 		if (game.jobs.Farmer.owned > 1000000) {
-			// if more than 200000 farmers allocate 3:1:4
+			// if more than 1000000 farmers allocate 3:1:4
 			if (game.jobs.Farmer.owned < game.jobs.Lumberjack.owned * 3 && game.jobs.Farmer.owned * 4 < 2 * game.jobs.Miner.owned) {
 				buyJob("Farmer");
 				tooltip("hide");
@@ -291,7 +293,7 @@ function sendTrimpsToWork() {
 				tooltip("hide");
 			}
 		} else if (game.jobs.Farmer.owned > 100000) {
-			// if more than 15000 farmers allocate 3:3:5
+			// if more than 100000 farmers allocate 3:3:5
 			if (game.jobs.Farmer.owned * 3 < game.jobs.Lumberjack.owned * 3 && game.jobs.Farmer.owned * 5 < 3 * game.jobs.Miner.owned) {
 				buyJob("Farmer");
 				tooltip("hide");
@@ -315,6 +317,7 @@ function sendTrimpsToWork() {
 				tooltip("hide");
 			}
 		}
+		game.global.buyAmt = buyAmt;
 	}
 }
 
